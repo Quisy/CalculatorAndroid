732573a34284585a1b6ce516fb17fbc9e0b34cc6
Quisy <lamprecht.mariusz@gmail.com>
2016-04-10 22:34:58 +0200

    Small changes in view

--- app/src/main/java/com/example/quisy/calculator/CalcAdvancedActivity.java
+++ app/src/main/java/com/example/quisy/calculator/CalcAdvancedActivity.java
@@ -1,7 +1,9 @@
 package com.example.quisy.calculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class CalcAdvancedActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class CalcAdvancedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc_advanced);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.CALC_TYPE);
     }
 }
